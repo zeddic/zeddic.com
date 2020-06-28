535edc4294c95d1250ae97bff036384af4a076a0
Scott Bailey <scott@zeddic.com>
2020-06-28 10:56:27 -0700

    Simple version of gatsby w/ markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,16 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sass`,
+  ],
+};
---
+++ gatsby-node.js
@@ -0,0 +1,54 @@
+const {createFilePath} = require(`gatsby-source-filesystem`);
+const path = require(`path`);
+
+exports.onCreateNode = ({node, getNode, actions}) => {
+  // Append the property 'slug' on every markdown node.
+  if (node.internal.type === `MarkdownRemark`) {
+    const {createNodeField} = actions;
+    const fileNode = getNode(node.parent);
+    const slug = createFilePath({node, getNode, basePath: `pages`});
+
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    });
+  }
+};
+
+exports.createPages = async ({actions, graphql, reporter}) => {
+  const {createPage} = actions;
+
+  // Dynamically create a page for every markdown file found.
+  const markdownTemplate = require.resolve(`./src/templates/markdown.tsx`);
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  // Handle errors
+  if (result.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`);
+    return;
+  }
+
+  result.data.allMarkdownRemark.edges.forEach(({node}) => {
+    createPage({
+      path: node.fields.slug,
+      component: markdownTemplate,
+      context: {
+        // additional data can be passed via context
+        slug: node.fields.slug,
+      },
+    });
+  });
+};
