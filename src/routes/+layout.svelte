64a59cb68d0d42eb3a91dd050c4fc1cbe3247582
Scott Bailey <scott@zeddic.com>
2023-01-19 23:08:35 -0800

    Update how fonts are imported to please vite.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script>
+  import '@fontsource/open-sans/700.css';
+  import '@fontsource/open-sans/500.css';
+  import '@fontsource/open-sans/400.css';
   import '/src/styles/global.scss';
 </script>
 
